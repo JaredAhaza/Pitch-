3ba6118314746efdd85a1661909fd3bb0d1cdeab
JaredAhaza <jaredahazq@gmail.com>
2018-05-24 21:16:01 +0300

    created route for pitches

--- app/main/views.py
+++ app/main/views.py
@@ -6,14 +6,14 @@ from flask_login import login_required, current_user
 from .. import db, photos
 
 
-@main.route('/')
-def index():
-    '''
-    Function that renders the index.html and its functions
-    '''
-    pitches = Pitch.query.all()
+# @main.route('/')
+# def index():
+#     '''
+#     Function that renders the index.html and its functions
+#     '''
+#     pitches = Pitch.query.all()
 
-    return render_template('index.html', pitches=pitches)
+#     return render_template('index.html', pitches=pitches)
 
 @main.route('/')
 def wired():
