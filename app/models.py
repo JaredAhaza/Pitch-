d2dfcfa0f1a7f5d3ad99e7ae06f0298f879ccc95
JaredAhaza <jaredahazq@gmail.com>
2018-05-24 21:09:02 +0300

    Created User class

--- app/models.py
+++ app/models.py
@@ -4,32 +4,7 @@ from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 
-class User(UserMixin, db.Model):
-    '''
-    defining subject
-    '''
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), index=True)
-    email = db.Column(db.String(255), unique=True, index=True)
-    password_hash = db.Column(db.String(255))
-    
-
-    @property
-    def password(self):
-        raise AttributeError('Cannot read the password')
-
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return f'Pitch {self.username}'
+#
 
 class Pitch(db.Model):
     '''
