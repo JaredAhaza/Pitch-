e4d376492f2f3ee168c4c540eff200db89db523c
JaredAhaza <jaredahazq@gmail.com>
2018-05-24 21:14:58 +0300

    initializing app

--- app/__init__.py
+++ app/__init__.py
@@ -18,27 +18,27 @@ bootstrap = Bootstrap()
 
 
 
-# def create_app(config_name):
+def create_app(config_name):
 
-#     #initializing app
-#     app = Flask(__name__)
+    #initializing app
+    app = Flask(__name__)
 
-#     app.config.from_object(config_options[config_name])
+    app.config.from_object(config_options[config_name])
 
-#     #initializing flask extensions
-#     db.init_app(app)
-#     login_manager.init_app(app)
-#     simple.init_app(app)
-#     bootstrap.init_app(app)
+    #initializing flask extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    simple.init_app(app)
+    bootstrap.init_app(app)
 
-#     #Registering blueprints
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
+    #Registering blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-#     from .auth import auth as auth_blueprint
-#     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-#     configure_uploads(app, photos)
+    configure_uploads(app, photos)
 
 
-#     return app
+    return app
