0b6469d22c4e167fafff67f1aaf7a1bb5d9ee658
JaredAhaza <jaredahazq@gmail.com>
2018-05-24 21:12:40 +0300

    imported neccesary files

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from config import config_options
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads, IMAGES
-from flask_simplemde import SimpleMDE
-from flask_bootstrap import Bootstrap
+# from flask import Flask
+# from config import config_options
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
+# from flask_uploads import UploadSet, configure_uploads, IMAGES
+# from flask_simplemde import SimpleMDE
+# from flask_bootstrap import Bootstrap
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
